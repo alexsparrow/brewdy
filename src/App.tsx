f51138febace4c41845b0e8107b15ea2a9b172e2
Alex Sparrow <alex@alexsparrow.dev>
2022-10-17 22:15:22 +0300

    Remove header and add screenshot

--- src/App.tsx
+++ src/App.tsx
@@ -112,13 +112,13 @@ const App = () => {
           )}
         </Disclosure>
 
-        <header className="bg-white shadow-sm">
+        {/* <header className="bg-white shadow-sm">
           <div className="mx-auto max-w-7xl py-4 px-4 sm:px-6 lg:px-8">
             <h1 className="text-lg font-semibold leading-6 text-gray-900">
               Dashboard
             </h1>
           </div>
-        </header>
+        </header> */}
         <main>
           <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <RecipeCreator />
