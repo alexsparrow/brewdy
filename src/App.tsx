552cccae6335e48e44b177b3319f532f011e5808
Alex Sparrow <alex@alexsparrow.dev>
2022-10-17 10:45:03 +0300

    Logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { RecipeCreator } from "./views/RecipeCreator";
 
 const navigation = [
   { name: "Home", href: "#", current: false },
-  { name: "Recipe", href: "#", current: true },
+  { name: "Recipes", href: "#", current: true },
   { name: "Settings", href: "#", current: false },
 ];
 
@@ -22,13 +22,21 @@ const App = () => {
               <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
                   <div className="flex items-center">
-                    <div className="flex-shrink-0">
+                    <div className="flex items-baseline h-full">
+                      <a
+                        href="/"
+                        className="text-2xl font-bold text-indigo-100 my-auto"
+                      >
+                        brewdy
+                      </a>
+                    </div>
+                    {/* <div className="flex-shrink-0">
                       <img
                         className="h-8 w-8"
                         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=300"
                         alt="Your Company"
                       />
-                    </div>
+                    </div> */}
                     <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
                         {navigation.map((item) => (
